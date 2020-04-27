5c509bf04d70e8e8b80f188633cec87378c024bb
Erick Santos <eksantos.jr@gmail.com>
2020-04-27 11:57:26 -0700

    Finish frontend setup

---
+++ frontend/peridot.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from './store'
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    return ReactDOM.render(<h1>Peridot!</h1>, root);
+    // const store = configureStore();
+    // return ReactDOM.render(<Root store={store} />, root);
+})
