e0ceb6c2958d110456d47f31b365906624c796e8
Erick Santos <eksantos.jr@gmail.com>
2020-04-27 17:03:27 -0700

    Add front end auth actions and reducers

--- frontend/peridot.jsx
+++ frontend/peridot.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import configureStore from './store'
+import configureStore from './store/store';
+
+//begin testing
+import {signup, login, logout} from './util/session_api_util';
+//end tetsting
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    return ReactDOM.render(<h1>Peridot!</h1>, root);
-    // const store = configureStore();
+    const store = configureStore();
+    
+    //begin testing 
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    window.signup = signup;
+    window.login = login;
+    window.logout = logout;
+    //end testing
+
     // return ReactDOM.render(<Root store={store} />, root);
+    return ReactDOM.render(<h1>Peridot!</h1>, root);
 })
