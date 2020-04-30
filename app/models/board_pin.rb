2858bcfaeb5a32a491ab065f3be73a8a07c6a827
Erick Santos <eksantos.jr@gmail.com>
2020-04-30 10:37:56 -0700

    Create board_pin model

---
+++ app/models/board_pin.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: board_pins
+#
+#  id         :bigint           not null, primary key
+#  board_id   :integer          not null
+#  pin_id     :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class BoardPin < ApplicationRecord
+    validates :board_id, :pin_id, presence: true
+    validates :board_id, :uniqueness => {:scope => :pin_id}
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_163005) do
+ActiveRecord::Schema.define(version: 2020_04_30_165707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2020_04_30_163005) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "board_pins", force: :cascade do |t|
+    t.integer "board_id", null: false
+    t.integer "pin_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id", "pin_id"], name: "index_board_pins_on_board_id_and_pin_id", unique: true
   end
 
   create_table "boards", force: :cascade do |t|
